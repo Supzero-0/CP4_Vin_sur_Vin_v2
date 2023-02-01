166b0667fe43ddffe5b6943ffb6999557bd2fc39
Supzero-0 <mat.lecanu@gmail.com>
2023-02-01 10:41:01 +0100

    creation of navbar, footer and homepage

---
+++ assets/js/collapse.js
@@ -0,0 +1,4 @@
+import { Collapse } from 'bootstrap'
+
+const collapseElementList = document.querySelectorAll('.collapse')
+const collapseList = [...collapseElementList].map(collapseEl => new Collapse(collapseEl))
